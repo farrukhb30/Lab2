eb6c7e93aefa12ea8d42ea86f991aeb1718bab13
Farrukh <farrukhb30@gmail.com>
2021-03-25 20:05:49 +0100

    Uppgift3 -- The List for employees is read and tested if the all the employees were in the list.

--- src/main/java/com/example/EmployeeRepositoryImpl.java
+++ src/main/java/com/example/EmployeeRepositoryImpl.java
@@ -1,16 +1,21 @@
 package com.example;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeRepositoryImpl implements EmployeeRepository {//In memory version with ArrayList
 
-    public List< Employee > employees = new ArrayList<>();
+    public List< Employee > employees = List.of(new Employee("Exec000", 2500.0),
+            new Employee("Exec001", 3500.0),
+            new Employee("Exec002", 4500.0),
+            new Employee("Exec003", 5500.0),
+            new Employee("Exec004", 6500.0),
+            new Employee("Exec005", 7500.0));
 
 
     @Override
     public List< Employee > findAll() {
-        return null;
+
+        return employees;
     }
 
     @Override
--- src/test/java/com/example/EmployeeRepositoryImplTest.java
+++ src/test/java/com/example/EmployeeRepositoryImplTest.java
@@ -1,5 +1,48 @@
 package com.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class EmployeeRepositoryImplTest {
 
+    @ParameterizedTest(name = "should return all the employees stored in ArrayList<>")
+    @MethodSource("findAllEmployees")
+    @DisplayName("using interface method findAll to get all employees stored")
+    void findAllEmployeesTest(Employee employeeId, boolean boolResult) {
+        EmployeeRepositoryImpl employeeRepositoryImpl = new EmployeeRepositoryImpl();
+
+        var list = employeeRepositoryImpl.findAll();
+        var idList = list.stream()
+                .map((employeeID) -> employeeID.getId())
+                .collect(Collectors.toList());
+
+        assertThat(idList).contains(employeeId.getId());
+    }
+
+    private static Stream< Arguments > findAllEmployees() {
+        EmployeeRepositoryImpl employeeRepositoryImpl = new EmployeeRepositoryImpl();
+        return Stream.of(
+                Arguments.of(employeeRepositoryImpl.employees.get(0), true),
+                Arguments.of(employeeRepositoryImpl.employees.get(1), true),
+                Arguments.of(employeeRepositoryImpl.employees.get(2), true),
+                Arguments.of(employeeRepositoryImpl.employees.get(3), true),
+                Arguments.of(employeeRepositoryImpl.employees.get(4), true),
+                Arguments.of(employeeRepositoryImpl.employees.get(5), true)
+
+        );
+    }
+
+    @Test
+    void saveEmployee()  {
+        EmployeeRepositoryImpl emp = new EmployeeRepositoryImpl();
+
+    }
 }
